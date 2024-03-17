f910da99924782637a72c7f98312a5ae2b33540d
BrayaCheat <123721732+BrayaCheat@users.noreply.github.com>
2024-03-17 18:42:49 +0700

    fetch api

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,16 +1,38 @@
 <!-- /products/:id -->
 
 <template>
-    <div>
-        <h1 class="text-4xl">Product Detail For {{ id }}</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, a.</p>
+    <div class="grid grid-cols-2 place-items-center p-9 border shadow-md">
+        <img :src="product.image" alt="" class="h-full w-80 object-contain">
+        <div class="grid gap-3">
+            <NuxtLink to="/products">Go Back</NuxtLink>
+            <h1>{{ product.category }}</h1>
+            <div class="flex items-center justify-between">
+                <h1 class="text-2xl">{{ product.title }}</h1>
+                <span class="text-xl text-red-600">${{ product.price }}</span>
+            </div>
+            <article>
+                {{ product.description }}
+            </article>
+        </div>
     </div>
 </template>
 
 <script setup>
+import axios from 'axios';
+
     const {id} = useRoute().params
+    const product = ref('')
     definePageMeta({
         layout: 'products'
+    })
+
+    onMounted(async () => {
+        try {
+            await axios.get(`https://fakestoreapi.com/products/${id}`)
+            .then((res) => product.value = res.data)
+        } catch (error) {
+            console.log(error)
+        }
     })
 </script>
 
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,18 +1,54 @@
 <!-- /products because index is the root file of products so it will display the root page when you access to this route-->
 
 <template>
-    <div>
-        <h1 class="text-4xl">Product Page</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, a.</p>
+  <div class="container mx-auto p-36 grid gap-9">
+    <h1 class="text-4xl">Product Page</h1>
+    <hr>
+
+    <div
+      v-if="products.length > 0"
+      class="grid grid-cols-4 place-items-stretch gap-9"
+    >
+      <NuxtLink
+        :to="`/products/${p.id}`"
+        v-for="p in products"
+        :key="p.id"
+        class="shadowm-md border p-9 grid gap-3"
+      >
+        <img :src="p.image" alt="" class="w-full h-80 object-contain" />
+        <hr />
+        <h1 class="text-md">{{ p.category }}</h1>
+        <div class="flex items-center justify-between">
+          <h1 class="text-2xl">{{ p.title.slice(0, 25) }}...</h1>
+          <span class="text-xl text-red-600">${{ p.price }}</span>
+        </div>
+        <p>{{ p.description.slice(0, 25) }}...</p>
+      </NuxtLink>
     </div>
+    <div v-else>
+      <h1 class="animated-pulse">Loading...</h1>
+    </div>
+  </div>
 </template>
 
 <script setup>
-    definePageMeta({
-        layout: 'products'
-    })
+import axios from "axios";
+
+definePageMeta({
+  layout: "products",
+});
+
+const products = ref([]);
+
+onMounted(async () => {
+  try {
+    await axios
+      .get("https://fakestoreapi.com/products")
+      .then((res) => (products.value = res.data));
+  } catch (error) {
+    console.log(error);
+  }
+});
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
