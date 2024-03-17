bb6813ff6e2453055ac6e68c3442b8f3092bb77c
BrayaCheat <123721732+BrayaCheat@users.noreply.github.com>
2024-03-17 23:39:13 +0700

    adding gap

--- components/product__card.vue
+++ components/product__card.vue
@@ -6,7 +6,7 @@
     <img :src="product.image" class="w-80 h-80 object-contain" />
     <hr />
     <h1 class="text-zinc-600">{{ product.category }}</h1>
-    <div class="flex items-center justify-between">
+    <div class="flex items-center justify-between gap-9">
       <h1 class="text-2xl font-bold">{{ (product.title).slice(0,15) }}</h1>
       <span class="font-medium text-xl text-primary">${{ product.price }}</span>
     </div>
